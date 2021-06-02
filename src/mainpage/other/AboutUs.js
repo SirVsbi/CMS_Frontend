a1de70d9b83bdb93b2ae8a84ee10436dd9505471
BogdanJWK <bogdanvasc.jwk@gmail.com>
2021-06-02 18:19:07 +0300

    More About info

--- src/mainpage/other/AboutUs.js
+++ src/mainpage/other/AboutUs.js
@@ -5,10 +5,19 @@ export default class AboutUs extends React.Component{
     render(){
         return (
             <div>
-                <strong><span>Team Fratii mei IntelliJ</span></strong>
-                <strong><span>Members:</span></strong>
+                <strong><span>Team Fratii mei IntelliJ</span></strong><br/>
+                <strong><span>Members:</span></strong><br/>
                 <span>
-                    Bogdan Vasc
+                    Vidam Szabolcs - leader<br/>
+                    Vasc Bogdan-Alexandru<br/>
+                    Tudorescu Alexandra<br/>
+                    Turcas Andrei<br/>
+                    Turcas David-Emil<br/>
+                    Andrea Barassa<br/>
+                </span><br/>
+                <strong><span>Teacher:</span></strong><br/>
+                <span>
+                    Alexandru Pirvu
                 </span>
             </div>
         )
